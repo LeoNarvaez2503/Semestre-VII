286569efd62ec9121d0f2f3db8c2623807875bde
LeoNarvaez2503 <leonardo_8.8@live.com>
2026-04-20 08:04:11 -0500

    feat: Add landing page for Bufete Legal Elite with HTML, CSS, and JavaScript functionality

---
+++ AseguramientoCalidad/Parcial I/Grupal/Act 2/landing-abogados/script.js
@@ -0,0 +1,7 @@
+function mostrarAlerta() {
+    alert('Gracias por tu interés. Pronto nos pondremos en contacto.');
+}
+
+function enviarFormulario() {
+    alert('Formulario enviado. Nos comunicaremos contigo pronto.');
+}
