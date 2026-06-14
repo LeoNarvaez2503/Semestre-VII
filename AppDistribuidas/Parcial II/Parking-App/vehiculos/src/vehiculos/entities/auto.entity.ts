75d25701d18bb7daa449aa8e85bf5ccca2e7d7c9
AnthonyMorales17 <aamorales17@espe.edu.ec>
2026-06-14 10:08:58 -0500

    feat: agregar proyecto de vehiculos

---
+++ AppDistribuidas/Parcial II/Parking-App/vehiculos/src/vehiculos/entities/auto.entity.ts
@@ -0,0 +1,13 @@
+import { ChildEntity, Column } from "typeorm";
+import { Vehiculo } from "./vehiculo.entity";
+
+@ChildEntity('Auto')
+export class Auto extends Vehiculo {
+
+    @Column()
+    numeroPuertas!: number;
+
+    obtenerTipo(): string {
+        return 'auto';
+    }
+}
