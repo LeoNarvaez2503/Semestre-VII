d5d9abf3058306bd662110500cc82b15f09ae8e7
Alejandro Caetano Flores Maya <caetano.flores@hotmail.com>
2026-07-14 18:58:49 -0500

    Arreglo bug login

--- Proyecto/usuarios-fast/app/main.py
+++ Proyecto/usuarios-fast/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import func
 from app.core.database import engine, Base, SessionLocal
 from app.routers import users, roles, auth
 from app.models.person import Person
